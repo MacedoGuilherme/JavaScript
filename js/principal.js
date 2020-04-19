b6938ebd900c182438fb639f6d6cd7e19df7dc26
Guilherme Macedo <macedoguilherme96@gmail.com>
2020-04-19 14:06:57 -0300

    Chegamos ao fim de mais um capítulo, e neste aqui utilizamos um pouco da lógica de programação e das funções do Javascript para validar nosso primeiro paciente.
    Neste capítulo vimos:
    
    O operador lógico de OU ( || )
    O operador lógico de E ( && )
    Busca de elementos

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,34 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector(".paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura);
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValido = true;
+
+if(peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdPeso.textContent = "Peso inválido!";
+}
+
+if(altura <= 0 || altura >= 3.00) {
+    console.log("Altura inválida!");
+    alturaEhValido = false;
+    tdAltura.textContent = ("Altura inválida!");
+}
+
+if(pesoEhValido && alturaEhValido) {
+    tdImc.textContent = imc;
+} else {
+    tdImc.textContent = "Altura e/ou peso inválidos!";
+}
