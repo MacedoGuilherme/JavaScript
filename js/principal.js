08708756e7d376169ef434ee141f329a650507a3
Guilherme Macedo <macedoguilherme96@gmail.com>
2020-04-24 17:02:55 -0300

    Neste capítulo aprendemos a lidar uma coleção de elementos do HTML e vimos:
    Como selecionar diversos elementos com a função querySelectorAll()
    Relembramos um método clássico de iteração com o for
    Vimos como manipular estilos com a propriedade .style
    Vimos que devemos utilizar camelCase quando queremos modificar uma propriedade que tenha duas palavras, como a background-color
    A boa prática de não alterar o estilo diretamente no Javascript e sim modificar as classes dos elementos com a propriedade .classList

--- js/principal.js
+++ js/principal.js
@@ -1,9 +1,13 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector(".paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
+for(var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
+    console.log(paciente);
+
+    var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 
 var tdAltura = paciente.querySelector(".info-altura");
@@ -19,16 +23,20 @@ if(peso <= 0 || peso >= 1000) {
     console.log("Peso inválido!");
     pesoEhValido = false;
     tdPeso.textContent = "Peso inválido!";
+    paciente.classList.add("paciente-invalido");
 }
 
 if(altura <= 0 || altura >= 3.00) {
     console.log("Altura inválida!");
     alturaEhValido = false;
     tdAltura.textContent = ("Altura inválida!");
+    paciente.classList.add("paciente-invalido");
 }
 
 if(pesoEhValido && alturaEhValido) {
-    tdImc.textContent = imc;
+    tdImc.textContent = imc.toFixed(2);
 } else {
     tdImc.textContent = "Altura e/ou peso inválidos!";
+    paciente.classList.add("paciente-invalido");
+}
 }
