e5fa2ee7af4ada32b3c09116251c6d435172784f
Guilherme Macedo <macedoguilherme96@gmail.com>
2020-05-04 21:15:07 -0300

    Neste capítulo vimos
    Dividir o código, separando por funcionalidades do sistema
    Os objetos em Javascript
    A quebrar funções grandes em funções menores ,com cada uma com sua responsabilidade
    A função form.reset() para limpar o formulário

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -34,42 +34,16 @@ if(altura <= 0 || altura >= 3.00) {
 }
 
 if(pesoEhValido && alturaEhValido) {
-    tdImc.textContent = imc.toFixed(2);
+    var imc = calculaImc(peso, altura);
+    tdImc.textContent = imc;
 } else {
     tdImc.textContent = "Altura e/ou peso inválidos!";
     paciente.classList.add("paciente-invalido");
 }
 }
 
-var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(event) {
-    event.preventDefault();
-    
-    var form = document.querySelector("#form-adiciona");
-
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
-
-    var pacienteTr = document.createElement("tr");
-
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-
-    var tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteTr);
-})
+function calculaImc(peso, altura) {
+    var imc = 0;
+    imc = peso / (altura * altura);
+    return imc.toFixed(2);
+}
